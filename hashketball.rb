52c89fdae9c2734cdff0eaa75ae7b45be90760f3
Brandon Green <brandon@brandonleeproductions.com>
2020-02-06 18:14:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,6 +153,9 @@ def game_hash
     #returns array of jersey numbers for that team
     game_hash.map do |place, team|
       if team[:team_name] == team_name
+        team.each do |attribute, data|
+          
+        end
       end
     end
   end
